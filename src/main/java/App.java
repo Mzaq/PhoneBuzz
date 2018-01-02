2618ad703e68e333dae021d2b1bfc255720e9a7a
Harish Veeramani <hdv987@gmail.com>
2018-01-02 17:37:55 -0600

    Working on Phase 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,11 @@
+import com.twilio.twiml.voice.Gather;
+
 import static spark.Spark.*;
 
 public class App {
     public static void main (String[] args){
         post("receive-call", ReceiveCall.call);
-
+        //get("receive-call", ReceiveCall.);
     }
 
 }
--- src/main/java/ReceiveCall.java
+++ src/main/java/ReceiveCall.java
@@ -1,16 +1,23 @@
 import static spark.Spark.post;
 
 import com.twilio.twiml.*;
+import com.twilio.twiml.voice.Gather;
 import com.twilio.twiml.voice.Say;
 import spark.Route;
 
 public class ReceiveCall {
 
     public static Route call = (request, response) -> {
-        Say sayMessage = new Say.Builder("Hello! Please enter a number for Fizz Buzz.").build();
-
-        VoiceResponse twiml = new VoiceResponse.Builder().say(sayMessage).build();
-
+        Say sayMessage = new Say.Builder("Hello! Please enter a number for Fizz Buzz. Enter # when finished.").build();
+        Gather input = new Gather.Builder().timeout(3).say(sayMessage).build();
+        VoiceResponse twiml = new VoiceResponse.Builder().gather(input).build();
+        System.out.println(twiml.toXml());
         return twiml.toXml();
         };
+
+    public static Route fizzBuzz = ((request, response) -> {
+
+        VoiceResponse twiml = new VoiceResponse.Builder().build();
+        return twiml.toXml();
+    });
 }
