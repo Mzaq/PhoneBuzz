fc17258f6549f3034ed6df5ac256f2d274dc0e07
Harish Veeramani <hdv987@gmail.com>
2018-01-02 19:03:22 -0600

    Working on Phase 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,7 @@ import static spark.Spark.*;
 public class App {
     public static void main (String[] args){
         post("/receive-call", ReceiveCall.call);
-        get("/receive-call", ReceiveCall.fizzBuzz);
+        post("/handle-number", ReceiveCall.fizzBuzz);
     }
 
 }
--- src/main/java/ReceiveCall.java
+++ src/main/java/ReceiveCall.java
@@ -1,23 +1,24 @@
 import static spark.Spark.post;
 
-import com.twilio.twiml.*;
+import com.twilio.http.HttpMethod;
 import com.twilio.twiml.voice.Gather;
 import com.twilio.twiml.voice.Say;
+import com.twilio.twiml.*;
 import spark.Route;
 
 public class ReceiveCall {
 
     public static Route call = (request, response) -> {
         Say sayMessage = new Say.Builder("Hello! Please enter a number for Fizz Buzz. Enter # when finished.").build();
-        Gather input = new Gather.Builder().timeout(3).say(sayMessage).build();
+        Gather input = new Gather.Builder().timeout(3).say(sayMessage).action("/handle-number").build();
         VoiceResponse twiml = new VoiceResponse.Builder().gather(input).build();
         //System.out.println(twiml.toXml());
         return twiml.toXml();
         };
 
     public static Route fizzBuzz = ((request, response) -> {
-        String[] params = request.splat();
-        int number = Integer.parseInt(params[0]);
+        String digit = request.params("Digits");
+        int number = Integer.parseInt(digit);
 
         Say message = fizzBuzz(number);
         VoiceResponse twiml = new VoiceResponse.Builder().say(message).build();
@@ -26,7 +27,6 @@ public class ReceiveCall {
 
     private static Say fizzBuzz(int number){
         String message = "";
-        VoiceResponse.Builder twiml = new VoiceResponse.Builder();
 
         for (int i = 1; i <= number; i++){
             if (i % 3 == 0 && i % 5 == 0) {
