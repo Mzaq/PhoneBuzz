39afa33a853f468e11840b21368f2bd4affef015
Harish Veeramani <hdv987@gmail.com>
2018-01-04 17:37:57 -0600

    Update template

--- src/main/java/controllers/Website.java
+++ src/main/java/controllers/Website.java
@@ -1,11 +1,18 @@
 package controllers;
 
 import spark.ModelAndView;
-import spark.Route;
 import spark.TemplateViewRoute;
+import util.Config;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Website {
-    public static TemplateViewRoute init = (request, response) -> new ModelAndView(new HashMap(), "templates/index.vtl");
+    //public static TemplateViewRoute init = (request, response) -> {new ModelAndView(model, "templates/index.vtl");
+
+    public static TemplateViewRoute init = ((request, response) -> {
+        Map<String, String> model = new HashMap<>();
+        model.put("twilioNumber", Config.TWILIO_NUMBER);
+        return new ModelAndView(model, "templates/index.vtl");
+    });
 }
