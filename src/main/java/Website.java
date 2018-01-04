d25296da81977261d2cc81337c110b0d572a3c6f
Harish Veeramani <hdv987@gmail.com>
2018-01-03 18:43:35 -0600

    Refactoring

--- src/main/java/Website.java
+++
@@ -1,9 +0,0 @@
-import spark.ModelAndView;
-import spark.Route;
-import spark.TemplateViewRoute;
-
-import java.util.HashMap;
-
-public class Website {
-    public static TemplateViewRoute init = (request, response) -> new ModelAndView(new HashMap(), "templates/index.vtl");
-}
